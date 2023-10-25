e79651b13470ab0f1136c39e0a87a6b1af7be8bf
David Miura <davidmiura@outlook.com.br>
2023-10-25 18:57:57 -0300

    Via

--- doses.js
+++ doses.js
@@ -1,15 +1,18 @@
 let divMedicamentos = document.getElementById("selectMedicamentos")
 let divDose = document.getElementById("doseKilo")
 
+let divVia = document.getElementById("via")
+
 fetch("doses.json").then((response) => {
     return response.json();
 }).then((dados) => {
     dados.medicamentos.map((medicamento) => {
-        divMedicamentos.innerHTML += `<option value="${medicamento.doseKilo}"> ${medicamento.nome} - ${medicamento.doseKilo}</option>`
+        divMedicamentos.innerHTML += `<option value="${medicamento.doseKilo}"> ${medicamento.nome} - ${medicamento.doseKilo} ml/Kg </option>`
         console.log(medicamento);
         console.log("==============================");
         console.log(medicamento.doseKilo);
 
+        // divVia.innerHTML = `${medicamento.nome}`        
     })
 }).catch((error) => {
     console.error('Algo deu errado!');
@@ -18,21 +21,29 @@ fetch("doses.json").then((response) => {
 
 
 function optionSelecionado() {
-
+    
     let select = document.getElementById('selectMedicamentos');
     let optionValue = select.options[select.selectedIndex];
-    let pesoAnimal = document.getElementById('pesoAnimal').value;
-    //let frequencia
-    //let via
 
-    let value = 0;
+    let pesoAnimal = document.getElementById('pesoAnimal').value;
+    
+    
+    // let value = 0;
     value = optionValue.value;
     value = value * pesoAnimal;
     console.log(value);
     
+    divDose.innerHTML = `${value} ml`;
     
     
-        divDose.innerHTML = ` Dose: ${value} ml`;
+    via = optionValue = select.options[select.selectedIndex];
+
+    
+    
+    
+    // divVia.innerHTML = `${medicamento.nome}`
+    divVia.innerHTML = `TESTE VIA ${via}`
+    
     
 }
 
